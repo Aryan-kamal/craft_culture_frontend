93d559f9175874378bcba8e8acfbeaaadf19bbbc
Aryan-kamal <kamalaryan1234567@gmail.com>
2025-06-04 00:36:11 +0530

    backendedit

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,3 @@
-export const API_URL='https://craftculturebackend-production.up.railway.app';
+export const API_URL = "https://craftculturebackend-production.up.railway.app";
+// http://localhost:5000
+// https://craftculturebackend-production.up.railway.app
