f56941f2e32158089dabf2884887732b0cd3605e
Aryan-kamal <kamalaryan1234567@gmail.com>
2025-06-06 01:26:45 +0530

    fixedprofiles

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://craftculturebackend-production.up.railway.app";
 // http://localhost:5000
 // https://craftculturebackend-production.up.railway.app
